a6c5de682dd97c49a9b772d5b2c1eb241ec029fe
Jacob Squires <jacobsquires05@gmail.com>
2025-07-06 04:55:41 +0000

    Add these for to connect to firebase:
    const firebaseConfig = {
      apiKey

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVTrBBhduUSuFxRZ_8okvS0JjV0EV2YuY",
+  authDomain: "veggie-box-p5g13.firebaseapp.com",
+  projectId: "veggie-box-p5g13",
+  storageBucket: "veggie-box-p5g13.firebasestorage.app",
+  messagingSenderId: "135857161584",
+  appId: "1:135857161584:web:17dee613a48ddf8f913bb5"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
